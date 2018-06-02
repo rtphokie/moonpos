96669b2107dc068d29c98f405db3595216d91a9b
trice <trice@cisco.com>
2018-06-02 15:05:44 -0400

    adding stub for config file

---
+++ twitter_config_example.py
@@ -0,0 +1,12 @@
+
+saturntwitter = { 'ConsumerKey': 'GOESHERE',
+                  'ConsumerSecret': 'GOESHERE',
+                  'AccessToken': 'GOESHERE-GOESHERE',
+                  'AccessTokenSecret': 'GOESHERE',
+                 }
+
+jupitertwitter = { 'ConsumerKey': 'GOESHERE',
+                   'ConsumerSecret': 'GOESHERE',
+                   'AccessToken': 'GOESHERE-GOESHERE',
+                   'AccessTokenSecret': 'GOESHERE',
+                   }
